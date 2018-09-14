3ece139be12d148bf39e6c66d8e81ba78f6edcb8
mksingh1388 <mithilesh.singh@wynk.in>
2018-09-14 20:50:33 +0530

    Initial Commit

---
+++ OrderPlacementApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OrderPlacementApp
+//
+//  Created by A1VNCFUR on 14/09/18.
+//  Copyright © 2018 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
